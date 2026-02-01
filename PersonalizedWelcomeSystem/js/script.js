908ecb1b80a122ca2fe3bf7631f72f6aaa9079e7
wsuits6 <wsuits6@gmail.com>
2026-02-01 09:15:26 +0000

    added 1.4

--- PersonalizedWelcomeSystem/js/script.js
+++ PersonalizedWelcomeSystem/js/script.js
@@ -1,4 +1,28 @@
 //problem explanation
+// =============================
+//  A function  that takes two parameter 
+// name and isMember
+// name is a String
+// ismember is a Boolean
 
 
-//setting varibales 
+//usinf conditionals
+//Welcome as VIP if the  are a member TRUE
+//Welcome them as normal user by NAME if they are Not member
+//If Guest and Member is True welcome them as VIP
+//================================
+
+// Call the function is the FOUR different Ways
+
+//(1) Creating the Function
+
+function welcomeUser(name = "Guest", isMember = false) {
+    //using the strict Equility Operator creat true conditional for isMember
+    if (isMember === true) {
+        console.log("Welcome VIP" + name)
+    } else {
+        // covers the isMember being fals and undefined scenario
+        console.log("Welcome " + name)
+    } 
+}
+
