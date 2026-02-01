824b13d9966e6d1396311820843dba663bd0f117
wsuits6 <wsuits6@gmail.com>
2026-02-01 09:19:19 +0000

    added 1.4 Update

--- PersonalizedWelcomeSystem/js/script.js
+++ PersonalizedWelcomeSystem/js/script.js
@@ -19,10 +19,21 @@
 function welcomeUser(name = "Guest", isMember = false) {
     //using the strict Equility Operator creat true conditional for isMember
     if (isMember === true) {
-        console.log("Welcome VIP" + name)
+        console.log("Welcome VIP " + name)
     } else {
         // covers the isMember being fals and undefined scenario
         console.log("Welcome " + name)
     } 
 }
 
+//teting function ooh ooohhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+// ===========================================================
+
+//welcome Wsuits6 as VIP
+welcomeUser("Wsuits6", true);
+
+//welcome Darkspectra as not a VIP
+welcomeUser("D4rk2p3ctr4", false );
+
+//welcome Guest
+welcomeUser()
